5adc996dc37153a13510e8e7d074abecdfc44c7a
Sebastian Rau <sebastian@s-rau.de>
2026-01-04 13:48:55 +0100

    chore: remove unused log

--- pkg/gui-fix/activation_stub.go
+++ pkg/gui-fix/activation_stub.go
@@ -3,8 +3,4 @@
 
 package guifix
 
-import "github.com/sebastianrau/focusrite-mackie-control/pkg/logger"
-
-var log *logger.CustomLogger = logger.WithPackage("gui-fix")
-
 func SetActivationPolicy() {}
